1f0fb2dbb1f5840f3293522d3ac4f2ccf1c1800c
David Davis <davisd@davisd.com>
2010-11-26 21:57:27 -0600

    added missing templatetags package in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='django-blogyall',
-    version='1.2.0',
+    version='1.2.1',
     author='David Davis',
     author_email='davisd@davisd.com',
-    packages=['blog',],
+    packages=['blog','blog.templatetags'],
     url='http://www.davisd.com/projects/django-blogyall',
     license='LICENSE',
     description='django-blogyall is the reusable blog application developed '\
